bf1f3a0ce70576057245e99800039e8a3b2156bf
BAD-99 <brandon.andrew.davis@gmail.com>
2023-11-14 03:17:22 -0700

    finished functions

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ const gridContainer = document.querySelector('.grid-container');
 
 function generateGrid(size){
     //remove old grid
+    gridContainer.textContent = '';
     let sqr = size * size;
     while(gridContainer.childElementCount > sqr){
         gridContainer.lastElementChild.remove();
     }
-    while(gridContainer.childElementCount < sqr){
+    for(i = 0; i < sqr; i++){
         let element = document.createElement('div');
         element.classList.add('grid-square')
+        element.style.width=`${100/size}%`;
+        element.style.height = element.style.height;
         gridContainer.appendChild(element);
     }
     //update grid width in css
@@ -17,8 +20,10 @@ function generateGrid(size){
 function changeGridSize(){
     let input = prompt("Set new grid size");
     parseInt(input);
-    if(input == Nan){return;}
-    input = Math.max(100, min(1, input));
-    generateGrid(100);
+    if(input == NaN){return;}
+    input = Math.min(100, Math.max(1, input));
+    generateGrid(input);
 }
 generateGrid(16);
+document.querySelector('button').addEventListener('click',
+()=>changeGridSize());
